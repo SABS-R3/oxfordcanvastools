591ca79f2f35160b346a65727ccb23a4c24d15b1
Fergus Cooper <fcooper8472@gmail.com>
2020-10-07 11:45:03 +0100

    Support assignments

---
+++ ReleaseNotes.md
@@ -0,0 +1,11 @@
+# Release Notes
+
+## v1.1
+
+- Add support for assignments
+- Update modules rather than recreating
+
+## v1.0
+
+- Regenerate a module
+- Get ID from file name
--- oxfordcanvastools/canvas_api.py
+++ oxfordcanvastools/canvas_api.py
@@ -72,52 +72,124 @@ def delete_module(course_id, module_id):
     logging.info('Deleting module {}'.format(course_id, module_id))
     payload = {}
     result = requests.delete(CANVAS_URL +
-                           '/api/v1/courses/{}/modules/{}'.format(course_id, module_id),
-                           data=payload, headers=AUTH_HEADER)
+                             '/api/v1/courses/{}/modules/{}'.format(course_id, module_id),
+                             data=payload, headers=AUTH_HEADER)
     result.raise_for_status()
     module = result.json()
 
     return module['id']
 
 
-def add_module_items(course_id, module_id, module_items):
+def add_module_items(course_id, module_id, module_items, assignments):
     logging.info('Adding module items {} to module {} in course {}'.format(
         module_items, module_id, course_id)
     )
     for item in module_items:
-        add_module_item(course_id, module_id, item)
+        add_module_item(course_id, module_id, item, assignments)
 
 
-def add_module_item(course_id, module_id, item, indent=0):
+def add_module_item(course_id, module_id, item, assignments, indent=0):
     logging.debug('Adding module item {} to module {} in course {}'.format(
         item, module_id, course_id)
     )
-    payload = {}
-    for k, v in item.items():
-        if k != 'sub_items':
-            payload['module_item[{}]'.format(k)] = v
-    payload['module_item[indent]'] = indent
 
-    result = requests.post(CANVAS_URL +
+    # replace content_id with canvas id we stored in assignments
+    if 'content_id' in item.keys():
+        for a in assignments:
+            if a['name'] == item['content_id']:
+                item['content_id'] = a['content_id']
+
+    payload = {'search_term': item['title']}
+    results = requests.get(CANVAS_URL +
                            '/api/v1/courses/{}/modules/{}/items'.format(
                                course_id, module_id
                            ),
                            data=payload, headers=AUTH_HEADER)
-    result.raise_for_status()
+    results.raise_for_status()
+
+    # check if module item already exists
+    payload = {}
+    for k, v in item.items():
+        if k == 'completion_requirement':
+            payload['module_item[{}][type]'.format(k)] = v['type']
+        else:
+            if k != 'sub_items':
+                payload['module_item[{}]'.format(k)] = v
+    payload['module_item[indent]'] = indent
+
+    result_json = results.json()
+    if len(result_json) == 0:
+        # create new item
+        result = requests.post(CANVAS_URL +
+                               '/api/v1/courses/{}/modules/{}/items'.format(
+                                   course_id, module_id
+                               ),
+                               data=payload, headers=AUTH_HEADER)
+        result.raise_for_status()
+    else:
+        # update existing item
+        item_id = result_json[0]['id']
+        result = requests.put(CANVAS_URL +
+                              '/api/v1/courses/{}/modules/{}/items/{}'.format(
+                                  course_id, module_id, item_id
+                              ),
+                              data=payload, headers=AUTH_HEADER)
+        result.raise_for_status()
 
     if 'sub_items' in item.keys():
         for sub_item in item['sub_items']:
-            add_module_item(course_id, module_id, sub_item, indent=indent+1)
+            add_module_item(course_id, module_id, sub_item, assignments, indent=indent + 1)
 
 
-def regenerate_module(course_sis_id, course_items):
+def add_assignment(course_id, assignment, position):
+    logging.debug('Adding assignment {} to course {}'.format(
+        assignment, course_id)
+    )
+    payload = {'search_term': assignment['name']}
+    results = requests.get(CANVAS_URL +
+                           '/api/v1/courses/{}/assignments'.format(course_id),
+                           data=payload, headers=AUTH_HEADER)
+    results.raise_for_status()
+
+    result_json = results.json()
+
+    payload = {}
+    for k, v in assignment.items():
+        if k != 'sub_items':
+            payload['assignment[{}]'.format(k)] = v
+    payload['assignment[position]'] = position
+
+    if len(result_json) == 0:
+        result = requests.post(CANVAS_URL +
+                               '/api/v1/courses/{}/assignments'.format(
+                                   course_id
+                               ),
+                               data=payload, headers=AUTH_HEADER)
+        result.raise_for_status()
+    else:
+        assignment_id = result_json[0]['id']
+        result = requests.put(CANVAS_URL +
+                              '/api/v1/courses/{}/assignments/{}'.format(
+                                  course_id, assignment_id
+                              ),
+                              data=payload, headers=AUTH_HEADER)
+        result.raise_for_status()
+
+    assignment['content_id'] = result.json()['id']
+
+
+def regenerate_module(course_sis_id, course_items, assignments):
     course_id = fetch_course_id(course_sis_id)
+    for i, a in enumerate(assignments):
+        add_assignment(course_id, a, i)
+
     for i in course_items:
         modules = search_modules(course_id, i['module_name'])
         if len(modules) > 1:
             raise RuntimeError('Too many modules matched name!')
-        if len(modules) == 1:
-            delete_module(course_id, modules[0]['id'])
+        if len(modules) == 0:
+            module_id = create_module(course_id, i['module_name'])
+        else:
+            module_id = modules[0]['id']
 
-        module_id = create_module(course_id, i['module_name'])
-        add_module_items(course_id, module_id, i['module_content'])
+        add_module_items(course_id, module_id, i['module_content'], assignments)
